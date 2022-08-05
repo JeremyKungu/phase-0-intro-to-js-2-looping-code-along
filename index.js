b943d9c945114974de3fd7e72e30090af43b08f4
JeremyKungu <jeremikungu27@gmail.com>
2022-08-05 10:57:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solutions in this file
+const names = ["Amy", "Abby", "Angie"];
+let event = "babyshower";
+
+function writeCards(names, event) {
+    const newArr = [];
+
+       for (let i = 0; i < names.length; i++) {
+        newArr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)       
+    }  
+
+    return newArr;   
+     
+}
+
+writeCards(names, event);
+
+function countDown () {
+    let i = 10;
+    while (i >= 0) {
+        console.log(i--);
+    }   
+}
+
+countDown();
+
+
+
